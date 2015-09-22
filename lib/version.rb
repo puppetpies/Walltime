1b75b5933f8bc46795101c0671b95926593d3c11
Bri in The Sky <brianh6854@googlemail.com>
2015-09-22 18:11:40 +0100

    Added version info

---
+++ lib/version.rb
@@ -0,0 +1,14 @@
+module Walltime #:nodoc:
+
+  module VERSION #:nodoc:
+
+    MAJOR = 0
+    MINOR = 0
+    TINY = 6
+    CODENAME = "Its all Giraffes my son!"
+
+    STRING = [MAJOR, MINOR, TINY].join('.')
+
+  end
+
+end
